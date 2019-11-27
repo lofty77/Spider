f0caa2dfb6581a5746d202ea256d4379696740fc
liang liang <liangliang1987429@163.com>
2019-11-27 08:17:05 -0800

    update2

--- test/test_url.py
+++ test/test_url.py
@@ -1,5 +1,11 @@
 from nose.tools import assert_equal
 
+import pytest
+
+import sys
+
+sys.path.append('../../')
+
 from Spider.spider import url
 
 
@@ -8,3 +14,7 @@ def test_get_chinese_city():
     city_chinese = url.get_chinese_city("tj")
 
     assert_equal(city_chinese, "天津")
+
+
+if __name__ == '__main__':
+    print(sys.path)
