c3c52fa600b49e3d1ae2b091d11af39ec8a47ab7
liang liang <liangliang1987429@163.com>
2019-12-02 22:54:24 -0800

    update

--- test/test_url.py
+++ test/test_url.py
@@ -1,10 +1,15 @@
 from nose.tools import assert_equal
 
 import pytest
-
 import sys
-
 sys.path.append('../')
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
 
 from spider import url
 
@@ -16,20 +21,20 @@ def test_get_chinese_city():
     assert_equal(city_chinese, "天津")
 
 
-
 def test_get_chinese_city_gbk():
     city_chinese_gbk = url.get_chinese_city_gbk("tj")
 
     assert_equal(city_chinese_gbk, "%CC%EC%BD%F2")
 
-# @pytest.fixture
-# def chrome_options(chrome_options):
-#     chrome_options.add_argument('--headless')
-#     return chrome_options
+
+@pytest.fixture
+def chrome_options(chrome_options):
+    chrome_options.add_argument('--headless')
+    return chrome_options
 
 
-# def test_open_web(selenium, chrome_options):
-#     selenium.get('http://www.baidu.com')
+def test_open_web(selenium, chrome_options):
+    selenium.get('http://www.baidu.com')
 
 
 if __name__ == '__main__':
