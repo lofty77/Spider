ccd0c3d2e0fc938c05c5a1774922d9ebac002e9c
liang liang <liangliang1987429@163.com>
2019-12-03 22:48:52 -0800

    update test_url.py

--- test/test_url.py
+++ test/test_url.py
@@ -7,6 +7,21 @@ sys.path.append('../')
 from spider import url
 
 
+def setup_function():
+    pass
+
+
+def teardown_function():
+   # This method is being called after each test case, and it will revert input back to original function
+    url.input = input
+
+
+def test_get_city():
+    url.input = lambda x: "tj"
+    output = url.get_city()
+    assert_equal(output, "tj")
+
+
 def test_get_chinese_city():
 
     city_chinese = url.get_chinese_city("tj")
@@ -20,5 +35,12 @@ def test_get_chinese_city_gbk():
     assert_equal(city_chinese_gbk, "%CC%EC%BD%F2")
 
 
+def test_get_date():
+    # Override the Python built-in input method
+    url.input = lambda x: "2019-01-01"
+    output = url.get_date("start")
+    assert_equal(output, "2019-01-01")
+
+
 if __name__ == '__main__':
-    pass
+    pytest.main(["-s", "test_url.py"])
